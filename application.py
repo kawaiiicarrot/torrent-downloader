b59a81b65f1c9cf8441111a68cfe8e2cdfd969e5
Shantanu Gupta <shantanugupta3048@gmail.com>
2022-08-08 00:46:07 -0400

    integrated error handling

--- application.py
+++ application.py
@@ -4,46 +4,13 @@ import tkinter as tk
 from tkinter import messagebox, filedialog, ttk
 import libtorrent as lt
 
-#define paths
-torrent_file = ''
-save_folder = ''
 
-#######################################################################################################################
-#defining button functions
-#######################################################################################################################
-#upload file function
-def uploadfile():
-    temp_file = filedialog.askopenfilename(title='Select a Torrent file', filetypes=[('torrent files', '*.torrent')])
-    global torrent_file
-    torrent_file = temp_file
-    file_str.set(torrent_file)
+#############################################################################################################################################
+#defining functionality
+#############################################################################################################################################
 
-#select save folder function
-def uploadfolder():
-    temp_folder = filedialog.askdirectory(title='Select a Folder to download the torrent')
-    global save_folder
-    save_folder = temp_folder + '/'
-    folder_str.set(save_folder)
-
-#download start function
-def download(file, folder):
-    #get magnet uri from entry box
-    magnet_uri = magnet.get()
-    
-    #create a torrent session
-    ses = lt.session({'listen_interfaces': '0.0.0.0:6881'})
-
-    #create torrent handle from magnet link or uploaded file
-    if magnet_uri != '':
-        params = lt.parse_magnet_uri(magnet_uri)
-        params.save_path = folder
-        handle = ses.add_torrent(params)
-
-    else:
-        params = {'save_path': folder}
-        params['ti'] = lt.torrent_info(file)
-        handle = ses.add_torrent(params)
-    
+#downloader function
+def downloader(handle):
     #store starting time
     begin = time.time()
 
@@ -86,12 +53,65 @@ def download(file, folder):
     main.update_idletasks()
     messagebox.showinfo('Success', 'Download Complete')
 
-#######################################################################################################################
+
+#############################################################################################################################################
+#defining button functions
+#############################################################################################################################################
+
+#define paths
+torrent_file = ''
+save_folder = ''
+
+#upload file function
+def uploadfile():
+    temp_file = filedialog.askopenfilename(title='Select a Torrent file', filetypes=[('torrent files', '*.torrent')])
+    global torrent_file
+    torrent_file = temp_file
+    file_str.set(torrent_file)
+
+#select save folder function
+def uploadfolder():
+    temp_folder = filedialog.askdirectory(title='Select a Folder to download the torrent')
+    global save_folder
+    save_folder = temp_folder + '/'
+    folder_str.set(save_folder)
+
+#download start function
+def download(file, folder):
+    #get magnet uri from entry box
+    magnet_uri = magnet.get()
+    
+    #create a torrent session
+    ses = lt.session({'listen_interfaces': '0.0.0.0:6881'})
+
+    #create torrent handle from magnet link or uploaded file
+    if magnet_uri != '':
+        try:
+            params = lt.parse_magnet_uri(magnet_uri)
+            params.save_path = folder
+            handle = ses.add_torrent(params)
+        except:
+            messagebox.showerror('Download Failed', 'Invalid Magnetic Link - Try Again')
+        else:
+            downloader(handle)
+    else:
+        params = {'save_path': folder}
+        try:
+            params['ti'] = lt.torrent_info(file)
+            handle = ses.add_torrent(params)
+        except:
+            messagebox.showerror('Download Failed', 'Invalid Torrent File - Try Again')
+        else:
+            downloader(handle)
+
+
+#############################################################################################################################################
 #GUI
-#######################################################################################################################
+#############################################################################################################################################
+
 #Root window
 root = tk.Tk()
-root.geometry('700x320')
+root.geometry('700x350')
 root.title('Torrent Downloader')
 #to be used when creating app for windows
 #root.iconbitmap('icon.ico')
@@ -101,52 +121,52 @@ main = tk.Frame(root)
 main.pack(padx=10, pady=10, fill='x', expand=True)
 
 #magnet_label
-magnet_label = tk.Label(main, text='Paste the Magnet Link here', font=('Courier', 16))
+magnet_label = tk.Label(main, text='Paste the Magnet Link here', font=('Montserrat', 15))
 magnet_label.pack(fill='x', expand=True)
 
 #magnet_url entry
 magnet_url = tk.StringVar()
-magnet = tk.Entry(main, textvariable=magnet_url)
+magnet = tk.Entry(main, textvariable=magnet_url, font=('Montserrat', 14))
 magnet.pack(fill='x', expand=True)
 
 #or_label
-or_label = tk.Label(main, text='OR', font=('Courier', 16))
-or_label.pack(fill='x', expand=True)
+or_label = tk.Label(main, text='OR', font=('Montserrat', 15))
+or_label.pack(fill='x', expand=True, pady=2)
 
 #file_label
-file_label = tk.Label(main, text='Select a Torrent file', font=('Courier', 16))
-file_label.pack(fill='x', expand=True)
+file_label = tk.Label(main, text='Select a Torrent file', font=('Montserrat', 15))
+file_label.pack(fill='x', expand=True, pady=2)
 
 #browse_file button
-browse_file_btn = tk.Button(main, text='Browse File', command=uploadfile, font=('Courier', 16))
+browse_file_btn = tk.Button(main, text='Browse File', command=uploadfile, font=('Montserrat', 15))
 browse_file_btn.pack(fill='x', expand=True)
 
 #file location label
 file_str = tk.StringVar()
-file_label = tk.Label(main, textvariable=file_str)
+file_label = tk.Label(main, textvariable=file_str, font=('Montserrat', 13))
 file_label.pack(fill='x', expand=True)
 
 #save_label
-save_label = tk.Label(main, text='Select a folder to download the torrent', font=('Courier', 16))
-save_label.pack(fill='x', expand=True)
+save_label = tk.Label(main, text='Select a folder to download the torrent', font=('Montserrat', 15))
+save_label.pack(fill='x', expand=True, pady=2)
 
 #browse_folder button
-browse_folder_btn = tk.Button(main, text='Browse Folder', command=uploadfolder, font=('Courier', 16))
+browse_folder_btn = tk.Button(main, text='Browse Folder', command=uploadfolder, font=('Montserrat', 15))
 browse_folder_btn.pack(fill='x', expand=True)
 
 #folder location label
 folder_str = tk.StringVar()
-folder_label = tk.Label(main, textvariable=folder_str)
+folder_label = tk.Label(main, textvariable=folder_str, font=('Montserrat', 13))
 folder_label.pack(fill='x', expand=True)
 
 #download button
-download_btn = tk.Button(main, text='Download', command=lambda: download(torrent_file, save_folder), font=('Courier', 16))
-download_btn.pack(fill='x', expand=True)
+download_btn = tk.Button(main, text='Download', command=lambda: download(torrent_file, save_folder), font=('Montserrat', 15))
+download_btn.pack(fill='x', expand=True, pady=5)
 
 #status_label
 status_str = tk.StringVar()
-status_label = tk.Label(main, textvariable=status_str)
-status_label.pack(fill='x', expand=True)
+status_label = tk.Label(main, textvariable=status_str, font=('Montserrat', 15))
+status_label.pack(fill='x', expand=True, pady=1)
 
 # progressbar
 pb = ttk.Progressbar(main, orient='horizontal', mode='determinate')
